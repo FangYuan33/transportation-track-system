8b923c3f19b8010f7d518e6cc6f93a293abfa16f
FangYuan <374072213@qq.com>
2022-12-29 21:03:23 +0800

    TTS 任务服务 initial

---
+++ tts-base/src/main/java/com/tts/base/iov/TtsIovTaskService.java
@@ -0,0 +1,13 @@
+package com.tts.base.iov;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * TTS任务服务
+ *
+ * @author FangYuan
+ * @since 2022-12-29 21:03:03
+ */
+@Service
+public class TtsIovTaskService {
+}
---
+++ tts-start/src/main/java/com/tts/runner/TtsApplicationRunner.java
@@ -0,0 +1,20 @@
+package com.tts.runner;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+
+/**
+ * TTS应用功能服务
+ *
+ * @author FangYuan
+ * @since 2022-12-29 20:57:46
+ */
+public class TtsApplicationRunner implements ApplicationRunner {
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        // 解密iov配置信息
+
+        // 启动IOV任务服务
+
+    }
+}
