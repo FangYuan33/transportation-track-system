c221a869083423f8d7747bcd78c4e0bd57734a71
FangYuan <374072213@qq.com>
2022-12-29 15:16:22 +0800

    添加TTS Node 节点启动程序

---
+++ tts-base/src/main/java/com/tts/base/application/TtsNodeRunner.java
@@ -0,0 +1,29 @@
+package com.tts.base.application;
+
+import com.tts.base.zookeeper.TtsZkNode;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+/**
+ * TTS节点启动类，随系统启动
+ *
+ * @author wangyilong13
+ * @since 2022-12-29 15:13:48
+ */
+@Slf4j
+@Component
+public class TtsNodeRunner implements ApplicationRunner {
+
+    @Autowired
+    private TtsZkNode ttsZkNode;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        log.info("TTS Node Runner start!!!");
+        ttsZkNode.start();
+        log.info("TTS Node Runner start over!!!");
+    }
+}
