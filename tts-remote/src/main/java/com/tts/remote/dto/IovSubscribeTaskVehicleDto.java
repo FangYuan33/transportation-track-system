5fb9ecf45be5a150795cc3fa46b0587bffef3503
FangYuan <374072213@qq.com>
2023-01-07 17:34:43 +0800

    iov task vehicle: 修复测试问题

--- tts-remote/src/main/java/com/tts/remote/dto/IovSubscribeTaskVehicleDto.java
+++ tts-remote/src/main/java/com/tts/remote/dto/IovSubscribeTaskVehicleDto.java
@@ -1,6 +1,9 @@
 package com.tts.remote.dto;
 
-public class IovSubscribeTaskVehicleDto {
+import java.io.Serializable;
+
+public class IovSubscribeTaskVehicleDto implements Serializable {
+    private static final long serialVersionUID = 1L;
 
     private String carrierCode;
 
