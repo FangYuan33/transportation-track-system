770ea1ac6a560a334b960323ee37689e52124b4c
FangYuan <374072213@qq.com>
2023-01-07 15:37:15 +0800

    iov task: sql

--- sql/initial.sql
+++ sql/initial.sql
@@ -29,10 +29,22 @@ CREATE TABLE `base_node_heartbeat_log`
 
 CREATE TABLE `iov_config`
 (
-    `id`                    bigint(20) NOT NULL AUTO_INCREMENT,
-    `iov_type`           varchar(15) DEFAULT NULL COMMENT 'iov类型',
-    `config_info` varchar(200)    DEFAULT NULL COMMENT '配置信息',
-    `create_time`           datetime NOT NULL COMMENT '创建时间',
-    `update_time`           datetime NOT NULL COMMENT '修改时间',
+    `id`          bigint(20) NOT NULL AUTO_INCREMENT,
+    `iov_type`    varchar(15)  DEFAULT NULL COMMENT 'iov类型',
+    `config_info` varchar(200) DEFAULT NULL COMMENT '配置信息',
+    `create_time` datetime NOT NULL COMMENT '创建时间',
+    `update_time` datetime NOT NULL COMMENT '修改时间',
     PRIMARY KEY (`id`) USING BTREE
 ) ENGINE=InnoDB COMMENT='Iov配置表';
+
+CREATE TABLE `iov_subscribe_task`
+(
+    `id`            bigint(20) NOT NULL AUTO_INCREMENT,
+    `iov_config_id` bigint(20) NOT NULL COMMENT 'iov配置ID',
+    `carrier_code`  varchar(20) NOT NULL COMMENT '承运商编码',
+    `server_name`   varchar(50) DEFAULT NULL COMMENT '服务名称',
+    `state`         int(2) NOT NULL COMMENT '任务状态: 10: 已启动待分配 20: 已分配 30: 运行中 40: 异常中断 50: 正常停止',
+    `create_time`   datetime    NOT NULL COMMENT '创建时间',
+    `update_time`   datetime    NOT NULL COMMENT '修改时间',
+    PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB COMMENT='Iov任务订阅表';
